2d38aa804a748d44d270ba132705db6555084858
kfirkfir89 <kfirkfir89@gmail.com>
2023-07-16 14:38:17 +0300

    server setup envierment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 function App() {
   return (
-    <div className="flex h-full w-full flex-col items-center bg-red-700">
-      <span className="flex bg-red-200 text-2xl font-semibold">
-        hello setupgfd
-      </span>
+    <div className="flex h-full w-full bg-black">
+      <div className="flex bg-red-300 p-2 px-4">hellfffffffffo</div>
+      <div className="flex flex-1 flex-col bg-red-500">
+        <div className="bg-green-200 p-2">fds</div>
+        <div className="h-full bg-green-700 p-2">fds</div>
+        <div className=" h-4/6 bg-green-300 p-2">fds</div>
+      </div>
     </div>
   );
 }
---
+++ server/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import fs from 'fs/promises'
+
+const app = express()
+
+app.get('/', function (req:any, res:any) {
+
+})
+
+app.listen(3000)
